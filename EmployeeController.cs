6ed60e5e831dfd040a23c682fc7e8059d2398e72
UshaMnd <53472516+Ushamnd@users.noreply.github.com>
2019-08-01 23:40:03 -0700

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public ViewResult Details(int id)
+        {
+            Employee model = _employeeRepository.GetEmployee(id);
+            return View(model);
+        }
+
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            new Employee
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department                
+            };
+            return View(employee);
+        }
+
+        // Through model binding, the action method parameter
+        // EmployeeEditViewModel receives the posted edit form data
+        [HttpPost]
+        public IActionResult Edit(Employee model)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+               
+                Employee updatedEmployee = _employeeRepository.Update(employee);
+
+                return RedirectToAction("index");
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {     
+           Employee employee = _employeeRepository.Delete(id);
+
+                return RedirectToAction("index");
+            
+        }
+
+    }
+}
